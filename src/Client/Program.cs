d25f0cfc46cd8849753a5779af56b7df3be0d43a
Vinay Arora <vin.aroar@gmail.com>
2023-01-18 23:45:26 +0530

    Initial commit

---
+++ src/Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using QuizManagerClientHosted.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("ServerAPI",
+      client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+  .CreateClient("ServerAPI"));
+
+
+// 👇 new code
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
+
+});
+// 👆 new code
+
+await builder.Build().RunAsync();
---
+++ src/Shared/QuizItem.cs
@@ -0,0 +1,14 @@
+namespace QuizManagerClientHosted.Shared;
+
+public class QuizItem
+{
+    public string Question { get; set; }
+    public List<string> Choices { get; set; }
+    public int AnswerIndex { get; set; }
+    public int Score { get; set; }
+
+    public QuizItem()
+    {
+        Choices = new List<string>();
+    }
+}
